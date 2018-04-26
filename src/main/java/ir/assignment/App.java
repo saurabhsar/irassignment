8e8a9c857e5d7eef610e39906357d6f2c1b0891b
Saurabh Araiyer <sizzsa@gmail.com>
2018-04-27 03:35:41 +0530

    Fixed a bug

--- src/main/java/ir/assignment/App.java
+++ src/main/java/ir/assignment/App.java
@@ -13,7 +13,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.Objects;
 
 import static ir.indexer.IndexLookup.parseAsAnd;
@@ -39,7 +38,7 @@ public class App
         }
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         while (true) {
-            System.out.println("Query Type:\n1. OR\n2. AND\n3. Print Index\n 4.Exit\nPlease enter selection (1 or 2 or 3 or 4) : ");
+            System.out.println("Query Type:\n1. OR\n2. AND\n3. Print Index\n4.Exit\nPlease enter selection (1 or 2 or 3 or 4) : ");
             String queryType = reader.readLine();
 //            searchResults = IndexLookup.getData("n alumni lane library ways to give find a person about us mission to develop and apply innovative techniques for efficient quantitative analysis and display of medical imaging data through interdisciplinary collaboration goals education to train physicians and technologists locally and");
 
@@ -58,7 +57,7 @@ public class App
                 searchResults = IndexLookup.getData(query);
                 System.out.println(parseAsAnd(searchResults, query));
             } else if (queryType.equalsIgnoreCase("3")) {
-                System.out.println(Arrays.toString(ObjectMapperFacade.getInstance().writeValueAsBytes(Index.documentMap)));
+                System.out.println(ObjectMapperFacade.getInstance().writeValueAsString(Index.documentMap));
             } else {
                 System.out.println("Exiting ad input is neither 1 or 2 or 3");
                 System.exit(0);
